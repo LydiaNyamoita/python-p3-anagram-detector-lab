c4c6c71233bc08c85d3817a5866842f3d4cd0666
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-08-24 05:39:35 +0300

    almost done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,30 @@
 # your code goes here!
+# your code goes here!
+
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = self.sort_word(self.word)
+
+    def sort_word(self, word):
+        return ''.join(sorted(word))
+    
+
+    def match(self, anagram_list):
+        anagram_matches = []
+
+        for anagram in anagram_list:
+            # self.word != anagram.lower() 
+            if self.sorted_word == self.sort_word(anagram.lower()):
+                anagram_matches.append(anagram)
+
+        return anagram_matches
+
+        
+
+  
+
+
+
+listen = Anagram("listen ")
